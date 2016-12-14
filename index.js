46c7a8612bf60c441a0c51fed3819eae95bb45c4
chenzezhang <yu521shao@163.com>
2016-12-14 23:57:10 +0800

    fsdf

--- index.js
+++ index.js
@@ -2,12 +2,31 @@
 const koa = require('koa');
 const app = new koa();
 
-const router = require('./router');
+//const router = require('./router');
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+
+const handlebars = require('handlebars');  
+
+app.use(async (ctx, next) => {
+  try {
+    await next(); // next is now a function
+  } catch (err) {
+    ctx.body = { message: err.message };
+    ctx.status = err.status || 500;
+  }
+});
+
 
 
 app.use(async (ctx, next) => {
 
-  ctx.body = 'Hello World';
+
+
+ ctx.body = "asfsadf"
+
   await next();
 });
 
