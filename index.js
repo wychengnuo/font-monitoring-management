731162f5dc9da9054835dc2b129b555a9ee44cf6
zhangchenze <zhangchenze@gomeholdings.com>
2017-01-17 17:38:06 +0800

    s

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ app.use(router());
 
 app.use(async (ctx, next) => {
   
-  try {
+    try {
 
-    await next(); // next is now a function
+        await next(); // next is now a function
 
-  } catch (err) {
-    ctx.locals = {};
-    ctx.status = err.status || ctx.res.statusCode || 500;
+    } catch (err) {
+        ctx.locals = {};
+        ctx.status = err.status || ctx.res.statusCode || 500;
     
-    ctx.err_path = path.join(__dirname, '/src/error/views/');
-    ctx.err_pub_path = path.join(__dirname, '/src');
+        ctx.err_path = path.join(__dirname, '/src/error/views/');
+        ctx.err_pub_path = path.join(__dirname, '/src');
     // ctx.locals = {   //本身的报错信息，可以不传，走自定义
     //         message: err.message
     // };  
-    require('./pretreatment')(ctx);
-  }
+        require('./pretreatment')(ctx);
+    }
 });
 
 
@@ -36,5 +36,5 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("http://%s:%s", host, port, ',启动成功');
+    console.log('http://%s:%s', host, port, ',启动成功');
 });
--- router.js
+++ router.js
@@ -8,12 +8,12 @@ glob
   .sync(`${__dirname}/src/**/router.js`)
   .filter((file) => (process.env.NODE_ENV !== 'production') || (path.dirname(file).indexOf('test') !== -1))
   .forEach((file) => {
-    try {
-      if (typeof require(file)() === 'function')
-          routers.push(require(file)());
-    } catch (error) {
-          
-    }
+      try {
+          if (typeof require(file)() === 'function')
+              routers.push;(require(file)());
+      } catch (error) {
+          console.log(error);
+      }
   });
 
 module.exports = () => compose(routers);
