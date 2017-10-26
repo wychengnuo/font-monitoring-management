0ddaccfa1ecc675c7669e604ea333d0e51420dd7
wangyan3@gomeholdings.com <wangyan3@gomeholdings.com>
2017-10-26 16:25:10 +0800

    修改分页，修改消息推送

--- src/public/typeError/index.js
+++ src/public/typeError/index.js
@@ -9,31 +9,6 @@ var ind = {
     init: function () {
         var _this = this;
         _this.getDate();
-
-        var options = {
-            bootstrapMajorVersion: 3, //版本
-            currentPage: _this.page, //当前页数
-            totalPages: _this.totalPage, //总页数
-            numberOfPages: 5,
-            itemTexts: function (type, page) {
-                switch (type) {
-                    case 'first':
-                        return '首页';
-                    case 'prev':
-                        return '上一页';
-                    case 'next':
-                        return '下一页';
-                    case 'last':
-                        return '末页';
-                    case 'page':
-                        return page;
-                }
-            },
-            onPageClicked: function (event, originalEvent, type, pages) {
-                _this.getDate();
-            }
-        };
-        $('#pageUl').bootstrapPaginator(options);
     },
     /**
      *  @param { object } 获取数据
@@ -84,6 +59,35 @@ var ind = {
                     $('#date').html(table);
 
                     _this.totalPage = data.pageSize; //取到pageCount的值
+
+                    if (pages == 1) {
+
+                        var options = {
+                            bootstrapMajorVersion: 3, //版本
+                            currentPage: _this.page, //当前页数
+                            totalPages: _this.totalPage, //总页数
+                            numberOfPages: 5,
+                            itemTexts: function (type, page) {
+                                switch (type) {
+                                    case 'first':
+                                        return '首页';
+                                    case 'prev':
+                                        return '上一页';
+                                    case 'next':
+                                        return '下一页';
+                                    case 'last':
+                                        return '末页';
+                                    case 'page':
+                                        return page;
+                                }
+                            },
+                            onPageClicked: function (event, originalEvent, type, pages) {
+                                _this.page = pages;
+                                _this.getDate();
+                            }
+                        };
+                        $('#pageUl').bootstrapPaginator(options);
+                    }
                 }
 
             },
