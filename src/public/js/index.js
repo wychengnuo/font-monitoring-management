3cfb57a5659ad363948afebef9c419a09352590b
MacBookPro-lyt <liuyutong@sgomeholdings.com>
2017-11-06 19:11:21 +0800

    优化样式 添加无数据显示

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -255,13 +255,12 @@ function histograms_urlErr(ele, originData) {
         };
     }
 
-    $('.channel select').change(function(){
-        var key = $('.channel select').val();
+    $('.url-channel select').change(function(){
+        var key = $('.url-channel select').val();
         groupCategories = []; groupColors = []
         var changedata = normalizeData(data[key]);
         myChart.setOption(getOption(changedata));
     })
-
 }
 
 function histograms_typeErr(ele, d, chartstype) {
@@ -315,7 +314,6 @@ function histograms_typeErr(ele, d, chartstype) {
         xAxis: [
             {
                 type: 'category',
-                // boundaryGap : false,
                 data: obj.xaxis
             }
         ],
@@ -343,6 +341,14 @@ function histograms_typeErr(ele, d, chartstype) {
         },
         series: obj.series
     };
+    if(chartstype == "line"){
+        option.xAxis[0].boundaryGap = false;
+        option.grid.left = "45%";
+        option.grid.tooltip.axisPointer.type = "line";
+        echarts.util.each(obj.series, function (item) {
+            item.areaStyle = {normal: {}};
+        })
+    }
     var myChart = echarts.init(document.getElementById(ele), 'macarons');
     myChart.setOption(option);
     setTimeout(function () {
@@ -352,58 +358,143 @@ function histograms_typeErr(ele, d, chartstype) {
     }, 200)
 }
 
-function histograms(ele, arr, arr1, str) {
-    var myChart = echarts.init(document.getElementById(ele));
-    myChart.title = '坐标轴刻度与标签对齐';
+function world_cloud(ele) {
 
     var option = {
-        color: ['#3398DB'],
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: { // 坐标轴指示器，坐标轴触发有效
-                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-            }
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: [{
-            type: 'category',
-            data: arr,
-            axisTick: {
-                alignWithLabel: true
-            }
-        }],
-        yAxis: [{
-            type: 'value'
-        }],
         series: [{
-            name: str ? str : '错误次数',
-            type: 'bar',
-            barWidth: '20%',
-            data: arr1
+            type: 'wordCloud',
+            gridSize: 2,
+            sizeRange: [12, 70],
+            rotationRange: [-90, 90],
+            shape: 'pentagon',
+            width: '80%',
+            height: '80%',
+            drawOutOfBound: true,
+            textStyle: {
+                normal: {
+                    color: function () {
+                        return 'rgb(' + [
+                            Math.round(Math.random() * 100 + 130),
+                            Math.round(Math.random() * 100 + 130),
+                            Math.round(Math.random() * 100 + 130)
+                        ].join(',') + ')';
+                    }
+                }
+            },
+            data: [
+                {
+                    name: "暂 无 数 据",
+                    value: 13000,
+                    textStyle: {
+                        fontfamily:'Microsoft YaHei',
+                        normal: {
+                            color: 'rgb(114,108,136)'
+                        }
+                    }
+                },
+                {
+                    name: "No data",
+                    value: 12181,
+                },
+                {
+                    name: "沒有數據",
+                    value: 4386,
+                },
+                {
+                    name: "データがない",
+                    value: 4055,
+                },
+                {
+                    name: "Il n’y a pas de données",
+                    value: 4055,
+                },
+                {
+                    name: "데이터 가 없다",
+                    value: 2467,
+                },
+                {
+                    name: "Нет данных",
+                    value: 2244,
+                },
+                {
+                    name: "Los datos no",
+                    value: 1898,
+                },
+                {
+                    name: "कोई डेटा नहीं",
+                    value: 1484,
+                },
+                {
+                    name: "nulla notitia",
+                    value: 1112,
+                },
+                {
+                    name: "ไม่มีข้อมูล",
+                    value: 965,
+                },
+                {
+                    name: "Sin datos",
+                    value: 847,
+                },
+                {
+                    name: "Nessun dato",
+                    value: 582,
+                },
+                {
+                    name: "Lewis Hamilton",
+                    value: 555,
+                },
+                {
+                    name: "Geen gegevens",
+                    value: 550,
+                },
+                {
+                    name: "Мэдээлэл байхгүй",
+                    value: 462,
+                },
+                {
+                    name: "Нема података",
+                    value: 366,
+                },
+                {
+                    name: "Ma'lumot yo'q",
+                    value: 360,
+                },
+                {
+                    name: "Không có dữ liệu",
+                    value: 282,
+                },
+                {
+                    name: "Keine Daten",
+                    value: 273,
+                },
+                {
+                    name: "Sem dados",
+                    value: 265,
+                }
+            ]
         }]
     };
-    myChart.setOption(option);
+    var chart = echarts.init(document.getElementById(ele));
+    chart.setOption(option);
+
 }
 
 $(function () {
-//typeErr 接口请求方法
     $.ajax({
         url: '/plugin/api/typeErr',
         type: 'GET',
         data: '',
         dataType: 'json',
         success: function (body) {
-            if (body.success) {
+            if (body.success && body.data.length != 0) {
                 histograms_typeErr('container_html', body.data, 'bar');
+            }else{
+                world_cloud('container_html');
             }
         },
         error: function (err) {
-            alert('暂无数据');
+            world_cloud('container_html');
         }
     });
     $.ajax({
@@ -411,14 +502,17 @@ $(function () {
         type: 'GET',
         data: '',
         dataType: 'json',
-        success: function (data) {
-            if (data.success) {
-                var d = data.data;
-                histograms_urlErr('container_interface', d, 'bar');
+        success: function (body) {
+            if (body.success && body.data.length != 0) {
+                var d = body.data;
+                $(".url-channel").css('display', 'block');
+                histograms_urlErr('container_interface', d);
+            }else{
+                world_cloud('container_interface');
             }
         },
         error: function (err) {
-            alert('暂无数据');
+            world_cloud('container_interface');
         }
     });
     $.ajax({
@@ -426,38 +520,17 @@ $(function () {
         type: 'GET',
         data: '',
         dataType: 'json',
-        success: function (data) {
-            if (data.success) {
-                var d = data.data;
+        success: function (body) {
+            if (body.success && body.data.length != 0) {
+                var d = body.data;
                 histograms_typeErr('container_downloads', d, 'line');
+            }else{
+                world_cloud('container_downloads');
             }
         },
         error: function (err) {
+            world_cloud('container_downloads');
         }
     });
-
-
-
-
-    // $.ajax({
-    //     url: '/plugin/api/getBrowser',
-    //     type: 'GET',
-    //     data: '',
-    //     dataType: 'json',
-    //     success: function (data) {
-    //         if (data.success) {
-    //             var d = data.data;
-    //             var arr = [], arr1 = [];
-    //             for (var i = 0; i < d.length; i++){
-    //                 arr.push(d[i].name);
-    //                 arr1.push(d[i].y);
-    //             }
-    //             histograms('container_browser', arr, arr1,'浏览器使用情况');
-    //         }
-    //     },
-    //     error: function (err) {
-    //         alert('暂无数据');
-    //     }
-    // });
 
 });
